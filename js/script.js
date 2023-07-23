00fb0b62d634e5edfa9fec922c9126edde63774f
Tobberz <34923362+Tobberz@users.noreply.github.com>
2023-07-23 22:09:09 +1000

    DataTable confusion

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-//DataTable
-new DataTable('#archive');
+new DataTable('#archive', {
+    ajax: 'https://api.jsonbin.io/v3/qs/64bd10709d312622a3836890',
+ 	columns: [
+		{data: 'name'},
+		{data: 'trailerLink'},
+		{data: 'pmcLink'},
+		{data: 'downloadJava'},
+		{data: 'downloadBedrock'},
+		{data: 'releaseDate'},
+		{data: 'updateDate'}
+	]
+});
