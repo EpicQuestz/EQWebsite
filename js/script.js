2ec8ddf48479e2b960c679a3b688fc204d25414b
Tobberz <34923362+Tobberz@users.noreply.github.com>
2023-07-26 12:59:13 +1000

    JSON BREAKTHROUGH!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-new DataTable('#archive', {
-    ajax: 'https://api.jsonbin.io/v3/qs/64bd10709d312622a3836890',
- 	columns: [
-		{data: 'name'},
-		{data: 'trailerLink'},
-		{data: 'pmcLink'},
-		{data: 'downloadJava'},
-		{data: 'downloadBedrock'},
-		{data: 'releaseDate'},
-		{data: 'updateDate'}
-	]
-});
+$(document).ready( function () {
+	$('#archive').DataTable({
+		ajax: {
+			url: "https://api.npoint.io/1cfe644e05ccb363a03e",
+			dataType: "json"
+		},
+		columns: [
+			{
+				data: 'name'
+			},
+			{
+				data: 'trailerLink',
+				render: function (data, type, row, meta) {
+					return `<a href="'+data+'">Trailer</a>`;
+				}
+			},
+			{
+				data: 'pmcLink'
+			},
+			{
+				data: 'downloadJava'
+			},
+			{
+			data: 'downloadBedrock'
+			},
+			{
+			data: 'releaseDate'
+			},
+			{data: 'updateDate'}
+		]
+	});
+} );
